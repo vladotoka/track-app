2e14119c58ad852a706df6a6a3b545af5e2f0a53
vladotoka <vladimir.k.terziev@tutanota.com>
2022-07-25 22:03:31 +0300

    expo init

---
+++ src/screens/AccountScreen.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+
+const AccountScreen = () => {
+  return (
+    <View>
+      <Text>AccountScreen</Text>
+    </View>
+  )
+};
+
+const styles = StyleSheet.create({});
+
+export default AccountScreen;
