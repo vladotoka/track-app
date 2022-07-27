c2b00427e4736e3bd6e686761609373827ba90bd
vladotoka <vladimir.k.terziev@tutanota.com>
2022-07-27 16:16:47 +0300

    signup, signin, signout dispatch

--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -7,8 +7,33 @@ const authReducer = (state, action) => {
     }
 };
 
+const signup = (dispatch) => {
+    return ({ email, password }) => {
+        // make api requst to sign up with that email and password
+
+        // if we sign up, modify our state, an say thath we are authenticated
+
+        // if signing up fails, we probably need to reflect an error message somewhere
+
+    };
+};
+
+const signin = (dispatch) => {
+    return ({ email, password }) => {
+        // Try to signin
+        // Handle success by updating state
+        // Handle failure by showing error message (somehow)
+    };
+};
+
+const signout = (dispatch) => {
+    return () => {
+        // somehow sign out
+    };
+};
+
 export const { Provider, Context } = createDataContext(
     authReducer,
-    {},
+    { signup, signin, signout },
     { isSignedIn: false }
 );
