f8b053a4133146c2e2df64d1f92fbfe7e158d797
vladotoka <vladimir.k.terziev@tutanota.com>
2022-07-27 17:58:06 +0300

    making an API request

---
+++ src/api/tracker.js
@@ -0,0 +1,5 @@
+import axios from "axios";
+
+export default axios.create({
+    baseURL: 'http://mean-banks-pay-45-84-184-212.loca.lt'
+});
--- src/screens/SignupScreen.js
+++ src/screens/SignupScreen.js
@@ -1,9 +1,11 @@
 import { StyleSheet, View } from 'react-native';
-import React, { useState } from 'react';
+import React, { useState, useContext } from 'react';
 import { Text, Input, Button } from '@rneui/themed';
 import Spacer from '../components/Spacer';
+import { Context as AuthContext } from '../context/AuthContext';
 
 const SignupScreen = ({ navigation }) => {
+  const { state, signup } = useContext(AuthContext);
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
 
@@ -31,7 +33,7 @@ const SignupScreen = ({ navigation }) => {
       />
 
       <Spacer>
-        <Button title="Sign Up" />
+        <Button title="Sign Up" onPress={() => signup({ email, password })} />
       </Spacer>
     </View>
   )
