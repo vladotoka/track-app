5c7a785e3eb7ca595c3e68cc29e34637e2e62a18
vladotoka <vladimir.k.terziev@tutanota.com>
2022-08-01 20:46:10 +0300

    location-ask for permiton, watch position fake coords

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,17 +1,33 @@
 import { StyleSheet, Text, View } from 'react-native';
 import React from 'react';
-import MapView from 'react-native-maps';
+import MapView, { Polyline } from 'react-native-maps';
 
 const Map = () => {
+  let points = [];
+  for (let i = 0; i < 10; i++) {
+    if(i%2 ===0) {
+      points.push({
+        latitude: 42.50606 + i * 0.001,
+        longitude: 27.46781 + i * 0.001
+      });
+    } else {
+      points.push({
+        latitude: 42.50606 - i * 0.0005,
+        longitude: 27.46781 + i * 0.001
+      });
+    }
+  }
   return (
     <MapView style={styles.map}
-    initialRegion={{
-      latitude: 42.50606,
-      longitude: 27.46781,
-      latitudeDelta: 0.0922,
-      longitudeDelta: 0.0421,
-    }}
-  />
+      initialRegion={{
+        latitude: 42.50606,
+        longitude: 27.46781,
+        latitudeDelta: 0.0461,
+        longitudeDelta: 0.02105,
+      }}
+    >
+      <Polyline strokeWidth={2} strokeColor="blue" coordinates={points} />
+    </MapView>
   )
 };
 
--- src/screens/TrackCreateScreen.js
+++ src/screens/TrackCreateScreen.js
@@ -1,15 +1,54 @@
+import '../_mockLocation';
 import { StyleSheet, View, SafeAreaView } from 'react-native';
-import React from 'react';
-import { Text, Input, Button } from '@rneui/themed';
+import React, { useState, useEffect, useCallback } from 'react';
+import { Text } from '@rneui/themed';
+import * as Location from 'expo-location';
+import { useFocusEffect } from '@react-navigation/native';
 
 import Map from '../components/Map';
 
 const TrackCreateScreen = () => {
+
+  const [err, setErr] = useState(null);
+  const startWatching = async () => {
+    try {
+      let { status } = await Location.requestForegroundPermissionsAsync();
+      if (status !== 'granted') {
+        throw new Error('Permission to access location was denied');
+      }
+
+      await Location.watchPositionAsync({
+        accuracy: Location.Accuracy.BestForNavigation,
+        timeInterval: 1000,
+        distanceInterval: 10
+      },
+        (loaction) => console.log(loaction));
+    } catch (e) {
+      setErr(e.message);
+    }
+  }
+
+  useEffect(() => {
+    startWatching();
+  }, []);
+
+  // useFocusEffect(
+  //   useCallback(async () => {
+  //     const subscribeWatchPosition = await Location.watchPositionAsync({
+  //       accuracy: Location.Accuracy.BestForNavigation,
+  //       timeInterval: 1000,
+  //       distanceInterval: 10
+  //     },
+  //       (loaction) => console.log(loaction));
+  //   }, [])
+  // );
+
   return (
     <SafeAreaView style={styles.container}>
       <View>
         <Text h3>Create a Track</Text>
         <Map />
+        {err && <Text>{err}</Text>}
       </View>
     </SafeAreaView>
   )
